3bb3502bf6e860a99eb429896ae74cf2d622af98
Priyansh Ramnani <priyanshramnani21@gmail.com>
2018-03-25 00:52:27 +0530

    Readded all files

---
+++ app/src/main/java/com/example/medifact/medifact/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.medifact.medifact;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                final Intent mainIntent = new Intent(MainActivity.this, Main2Activity.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, 5000);
+
+
+    }
+
+}
+
