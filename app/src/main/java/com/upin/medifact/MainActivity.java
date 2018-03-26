3e6a4129ee2fbf269c8e9c713705b7fb7ffc8c2a
Urmil Shroff <urmilshroff@gmail.com>
2018-03-26 19:31:59 +0530

    Fixed errors during recreation of project

--- app/src/main/java/com/upin/medifact/MainActivity.java
+++ app/src/main/java/com/upin/medifact/MainActivity.java
@@ -1,13 +1,28 @@
 package com.upin.medifact;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                final Intent mainIntent = new Intent(MainActivity.this, com.upin.medifact.Main2Activity.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, 5000);
+
     }
-}
