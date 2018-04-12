7635e62aee5713189c6593dfff0491801b3a7689
Urmil Shroff <urmilshroff@gmail.com>
2018-04-12 23:55:10 +0530

    FirebaseAuth working

--- Firebase/app/src/main/java/com/upin/firebase/MainActivity.java
+++ Firebase/app/src/main/java/com/upin/firebase/MainActivity.java
@@ -1,5 +1,7 @@
 package com.upin.firebase;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,22 +9,35 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity
 {
-String name,email,password;
+public String name,email,password;
+public ProgressDialog loader;
+public FirebaseAuth authenticator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loader=new ProgressDialog(this);
+
+        authenticator=FirebaseAuth.getInstance();
+
         login();
     }
 
     public void login()
     {
         final EditText name=findViewById(R.id.nameInput);
-        EditText email=findViewById(R.id.emailInput);
-        EditText password=findViewById(R.id.passInput);
+        final EditText email=findViewById(R.id.emailInput);
+        final EditText password=findViewById(R.id.passInput);
 
         Button b=findViewById(R.id.loginButton);
 
@@ -31,13 +46,17 @@ String name,email,password;
             @Override
             public void onClick(View view)
             {
-                if(name.getText().toString().equals(""))
+                if(name.getText().toString().isEmpty())
                 {
                     Toast.makeText(MainActivity.this, "Please enter a valid name", Toast.LENGTH_SHORT).show();
                 }
                 else
                 {
-                    Toast.makeText(MainActivity.this, "Login success!", Toast.LENGTH_SHORT).show();
+                    loader.setMessage("Registering "+name+"...");
+                    loader.show();
+
+                    authenticator.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString());
+
                 }
             }
         });
