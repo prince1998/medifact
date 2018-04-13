1f12c6829a6403a7f0fd2e74eb4cfcb1820f4a07
Priyansh Ramnani <priyanshramnani21@gmail.com>
2018-04-13 10:23:12 +0530

    Added currently working project

---
+++ MediFact/app/src/main/java/medifact/upin/medifact/Pedometer.java
@@ -0,0 +1,21 @@
+package medifact.upin.medifact;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Pedometer extends Fragment
+{
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.pedometer,container,false);
+        return myView;
+
+    }
+}
+
